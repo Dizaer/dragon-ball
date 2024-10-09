d705c99197a9b71681ae0b6ef2cb065f41d9db32
IE Dev 2 <ie.dev.2@hotmail.com>
2024-10-09 10:13:30 -0700

    Arreglo agrupado ahora se crea con método reduce

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -3,7 +3,6 @@ import { CharactersAPIService } from './characters-api.service';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { HeaderComponent } from '../header/header.component';
 import { LogoComponent } from '../logo/logo.component';
 
 @Component({
@@ -16,64 +15,53 @@ import { LogoComponent } from '../logo/logo.component';
     LogoComponent
   ],
   templateUrl: './characters.component.html',
-  styleUrl: './characters.component.css'
+  styleUrl: './characters.component.css',
 })
-export class CharactersComponent implements OnInit{
-  charactersData:any[] = [];
-  afiliacionez:any[] = [];
+
+export class CharactersComponent implements OnInit {
+  charactersData: any[] = [];
+  afiliacionez: any[] = [];
   filter: any;
-    
+
   constructor(
     private charactersAPI: CharactersAPIService,
     private fb: FormBuilder
-  ){
+  ) {
     this.filter = this.fb.group({
-      limit:['58', [Validators.max(100)]],
-      affiliation:[''],
-    })
+      limit: ['58', [Validators.max(100)]],
+      affiliation: [''],
+    });
   }
 
   ngOnInit(): void {
     this.cargarData();
   }
-    
-  cargarData(){
+
+  cargarData() {
     const filtro = this.filter.value;
-    this.charactersAPI.obtener(filtro).subscribe((res:any)=>{
+    this.charactersAPI.obtener(filtro).subscribe((res: any) => {
       this.charactersData = filtro.affiliation ? res : res.items;
 
-      this.charactersData.forEach(item => {
-        const afiliacion = item.affiliation;
-        if (!this.afiliacionez[afiliacion]){
-          this.afiliacionez[afiliacion] = [];
+      this.afiliacionez = this.charactersData.reduce((obj, item) => {
+        const affiliation = item.affiliation;
+        if (!obj[affiliation]) {
+          obj[affiliation] = [];
         }
-
-        this.afiliacionez[afiliacion].push(item);
-      });
-
-      console.log(this.afiliacionez)
-      console.log(this.charactersData
-        .map(item => item.affiliation)
-        .reduce((obj, affiliation) => {
-          if (obj[affiliation]) {
-            obj[affiliation] = obj[affiliation] + 1;
-          } else {
-            obj[affiliation] = 1;
-          }
-          return obj;
-        }, {}));
+        obj[affiliation].push(item);
+        return obj;
+      }, {});
+      console.log(this.afiliacionez);
     });
-   }
+  }
 
-  filtrar(){
+  filtrar() {
     console.log(this.filter.value);
     this.cargarData();
   }
 
-  showCharacter(id: number){
-    this.charactersAPI.obtenerPersonaje(id).subscribe((res:any)=>{
+  showCharacter(id: number) {
+    this.charactersAPI.obtenerPersonaje(id).subscribe((res: any) => {
       console.log(res);
     });
   }
-
 }
